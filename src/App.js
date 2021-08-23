319346b48a5d191f906b00302d576e7bc49e3890
Samuel Heluany <samuelheluany@hotmail.com>
2021-08-23 19:08:14 -0300

    secound commit

--- src/App.js
+++ src/App.js
@@ -3,36 +3,33 @@ import './App.css';
 import axios from 'axios'
 
 function App() {
-  const [pokemon, setPokemon] = useState()
+  const [pokemons, setPokemons] = useState([])
+
 
   useEffect(() => {
-    const pokeItems = async () => {
-      const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/`)
-      setPokemon(result) 
+    const pokeItems = async (id) => {
+      const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      setPokemons(result)
+      console.log(result)
     }
     pokeItems()
   }, [])
 
+
+ 
   return (
     <div className="container">
       <div className="name">
         <h2>Pokemon API</h2>
-        <input type="text" placeholder="Digite o nome do Pokemon" autoFocus="On"/>
-        <button>Procurar</button>
       </div>
       <div className="pokemons">
-        {setPokemon.map((item) => {
-          const { name, img, attack, defense} = item
-          return (
-            <div className="item">
-              <div className="image">
-                <img src={img} alt={name} />
-              </div>
-            </div>
-          )
-        })}
+          <div className="pokemons">
+          </div>
+        
       </div>
-        </div>
-  );
-      }
+    </div>
+  )
+}
+
+
 export default App;
