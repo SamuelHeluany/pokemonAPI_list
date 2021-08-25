c18c58cace98f004c1b41043297c9a43fcc7ffc1
Samuel Heluany <samuelheluany@hotmail.com>
2021-08-25 10:10:45 -0300

    third commit

--- src/App.js
+++ src/App.js
@@ -8,24 +8,33 @@ function App() {
 
   useEffect(() => {
     const pokeItems = async (id) => {
-      const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
-      setPokemons(result)
+      const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/`)
+      setPokemons(result.data.results)
       console.log(result)
     }
     pokeItems()
   }, [])
 
 
- 
+
   return (
     <div className="container">
       <div className="name">
         <h2>Pokemon API</h2>
       </div>
       <div className="pokemons">
-          <div className="pokemons">
-          </div>
-        
+        <div className="pokemons">
+
+          {pokemons.map((poke) => {
+            return (
+              <div className="pokename">
+                <h4>{poke.name}</h4>
+              </div>
+            )
+          })}
+        </div>
+
+
       </div>
     </div>
   )
